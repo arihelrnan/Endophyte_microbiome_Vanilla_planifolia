5d2c6964e4f16c151bffc6c84ef716281290aae8
arihelrnan <71803692+arihelrnan@users.noreply.github.com>
2020-11-13 20:50:13 -0600

    Update ITS_paired.sh

--- bin/amptk/ITS_paired.sh
+++ bin/amptk/ITS_paired.sh
@@ -11,4 +11,4 @@ amptk cluster -i ITS_merged.demux.fq -o ITS_cluster --uchime_ref ITS -m 10
 amptk filter -i ITS_cluster.otu_table.txt -f ITS_cluster.cluster.otus.fa -o ITS_filter -p 0.005 --min_reads_otu 10
 
 #Assign taxonomy to OTUs
-amptk taxonomy -f ITS_filter.filtered.otus.fa -i ITS_filter.final.txt -o ITS_taxonomy -m mapping_file.txt -d ITS2 --tax_filter Fungi(base) arihel@pavilion:/hdd/arihel/se
+amptk taxonomy -f ITS_filter.filtered.otus.fa -i ITS_filter.final.txt -o ITS_taxonomy -m mapping_file.txt -d ITS2 --tax_filter Fungi
