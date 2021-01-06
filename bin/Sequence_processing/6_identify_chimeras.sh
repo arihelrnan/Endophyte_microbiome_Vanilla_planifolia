09fbd4489d3f491e0cb2cad78f7ef1e5ca8a4ddd
arihel <arihems@live.com.mx>
2021-01-06 16:22:04 -0600

    Add amptk scripts

--- bin/Sequence_processing/6_identify_chimeras.sh
+++
@@ -1,7 +0,0 @@
-#Identify chimeras sequence of 16SrRNA data using uchime according 'identify_chimeras_parameters_16S.txt' file 
-
-mothur identify_chimeras_parameters_16S.txt
-
-#Identify chimeras sequence of ITS data using uchime 'according identify_chimeras_parameters_ITS.txt' file
-
-mothur identify_chimeras_parameters_ITS.txt
