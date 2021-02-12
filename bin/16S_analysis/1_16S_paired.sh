52b6238624ed54c0f5000c9b2eb1dfafa580e218
arihelrnan <71803692+arihelrnan@users.noreply.github.com>
2021-02-11 20:34:15 -0600

    Update 1_16S_paired.sh

--- bin/16S_analysis/1_16S_paired.sh
+++ bin/16S_analysis/1_16S_paired.sh
@@ -12,3 +12,9 @@ amptk filter -i 16S_cluster.otu_table.txt -f 16S_cluster.cluster.otus.fa -o 16S_
 
 #Assign taxonomy to OTUs
 amptk taxonomy -f 16S_filter.filtered.otus.fa -i 16S_filter.final.txt -o 16S_taxonomy -m 16S_merged.mapping_file.txt -d 16S --tax_filter Bacteria
+
+#Convert OTU Table with .txt format to biom format
+biom convert -i 16S_taxonomy.otu_table.taxonomy.txt -o 16S_amptk.biom --to-hdf5 --table-type="OTU table" --process-obs-metadata taxonomy
+
+#Filter OTU classify in Class Chloroplast and Family Mitochondria in 16S_amptk.biom using argument filter_taxa_from_otu_table.py in QIIME
+filter_taxa_from_otu_table.py -i 16S_amptk.biom -o 16S.otu_table.taxonomy.biom -n c__Chloroplast,f__Mitochondria
